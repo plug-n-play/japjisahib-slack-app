cf7c24218fe9b551cd29a35ed98705c53f6dd5ad
plug-n-play <>
2019-11-09 01:50:02 +0530

    .

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 export default (req, res) => {
-  const date = new Date().toISOString()
-    .replace(/T/, ' ')
-    .replace(/\..+/, '')
 
-  res.json({ date })
+  const response_type = 'in_channel'
+  // const message = 'If you haven\'t failed, you haven\'t tried'
+  const message = 'Ik Onkar'
+
+  res.json({ response_type, text: message })
 }
